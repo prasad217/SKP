fa2b0d8fc82c3cd7e5b1ec98f515e1dd0c40c425
PRASAD_MEKALA <103354571+prasad217@users.noreply.github.com>
2023-11-13 13:29:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,24 @@ const images = [
 
 let currentIndex = 0;
 const imageElement = document.getElementById('mainImage');
+const transitionDuration = 5000; // 5 seconds
 
 function changeImage() {
-    imageElement.style.transition = 'transform 2s';
+    if (!imageElement || !images || images.length === 0) {
+        console.error("Invalid image element or images array");
+        return;
+    }
+
+    imageElement.style.transition = `transform ${transitionDuration / 1000}s`;
     imageElement.style.transform = 'translateX(-100%)';
 
     setTimeout(() => {
         currentIndex = (currentIndex + 1) % images.length;
         imageElement.src = images[currentIndex];
         imageElement.style.transform = 'translateX(0)';
-    }, 200); // A small delay to allow the transition property to take effect
+    }, transitionDuration + 20); // Allowing 20ms more for a smoother transition
 }
+
+// Call changeImage() every 5 seconds
+setInterval(changeImage, transitionDuration);
 
